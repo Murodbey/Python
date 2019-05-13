03003c4155dfb26341104d5dfa0fb4cd218f198e
Murodbey <abdullayevmurod@gmail.com>
2019-05-13 17:38:20 -0500

    using_file

--- using_file.py
+++ using_file.py
@@ -2,7 +2,7 @@ my_file = open ('xmen.txt', 'w+t')
 my_file.write('Beast\n')
 my_file.write('Phoenix\n')
 my_file.writelines([
-    'Cyclops',
-    'Bishop',
-    'Nightcrawler',
+    'Cyclops\n',
+    'Bishop\n',
+    'Nightcrawler\n',
 ])
