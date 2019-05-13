d2a859140b17e6b079cc18196b1bb976e86fc4dd
Murodbey <abdullayevmurod@gmail.com>
2019-05-13 17:55:48 -0500

    using_file

--- using_file.py
+++ using_file.py
@@ -1,15 +1,15 @@
-my_file = open ('xmen.txt', 'w+t')
-my_file.write('Beast\n')
-my_file.write('Phoenix\n')
-my_file.writelines([
-    'Cyclops\n',
-    'Bishop\n',
-    'Nightcrawler\n',
-])
+with open ('xmen.txt', 'w+t') as my_file:
+    my_file.write('Beast\n')
+    my_file.write('Phoenix\n')
+    my_file.writelines([
+        'Cyclops\n',
+        'Bishop\n',
+        'Nightcrawler\n',
+    ])
 my_file.close()
 
 my_file = open('xmen.txt', 'r')
-print(my_file.read())
-print("I`m reading again")
-print(my_file.read())
-my_file.close()
+with my_file:
+    print(my_file.read())
+    print("I`m reading again")
+    print(my_file.read())
