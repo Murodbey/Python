9ca54da9f0d76abeaf163192cb8e776826d9a6b8
Murodbey <abdullayevmurod@gmail.com>
2019-05-13 18:14:35 -0500

    updated

--- handle_errors.py
+++ handle_errors.py
@@ -1,5 +1,3 @@
-import sys
-
 file_name = 'recipes.txt'
 
 try:    
@@ -8,7 +6,9 @@ try:
     my_file.close()
 except FileExistsError as err:
     print(f"The {file_name} file already exists.")
-    sys.exit(1)
 except:
     print(f"Unable to write to the file")
-    sys.exit(1)
+else:
+    print(f"Wrote to {file_name}")
+finally:
+    print("Execution completed")
