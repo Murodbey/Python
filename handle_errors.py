e4380576ab5dfbbb5770135e0c1d773beec658ed
Murodbey <abdullayevmurod@gmail.com>
2019-05-13 18:12:03 -0500

    updated

--- handle_errors.py
+++ handle_errors.py
@@ -6,6 +6,9 @@ try:
     my_file = open(file_name, 'x')
     my_file.write('Meatballs\n')
     my_file.close()
-except:
+except FileExistsError as err:
     print(f"The {file_name} file already exists.")
+    sys.exit(1)
+except:
+    print(f"Unable to write to the file")
     sys.exit(1)
